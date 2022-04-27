18c51e28bb58200035b106e9ae7a9598fafcf37c
Lara Filippone <lara.filippone@gmail.com>
2022-04-27 10:30:06 +0200

    created palette.scss

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@ import HeaderComponent from './components/HeaderComponent.vue'
 import MainComponent from './components/MainComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
 
-
 export default {
   name: 'App',
   components: {
@@ -30,8 +29,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,16 +1,31 @@
 <template>
-    <div>
-        TOPHEADER
+    <div class="top-header">
+        <div class="contact">
+            <span>Call us for a Free Quote: </span>
+            <a href="tel:18005556709">1.800.555.6789</a>
+
+        </div>
+        <div class="social"></div>
     </div>
 </template>
 
 <script>
+
+
 export default {
     name: 'TopHeader'
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/palette.scss";
+
+.top-header{
+    background-color: #69bf14;
+    height: 100px;
+    color: $white;
+
+}
 
 </style>
