2aadba95ab220e1b1e3691c3d359278f3da1485f
Lara Filippone <lara.filippone@gmail.com>
2022-04-28 10:54:06 +0200

    create MoveComponent and imported in mainComponent

---
+++ src/components/MoveComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">move</div>
+</template>
+
+<script>
+export default {
+    name: 'MoveComponent'
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/palette.scss";
+@import "../assets/elements.scss";
+
+</style>
