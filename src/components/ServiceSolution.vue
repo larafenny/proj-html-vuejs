27013df19c32ba284990b5d21a435229b06c3a27
Lara Filippone <lara.filippone@gmail.com>
2022-04-28 10:23:04 +0200

    created serviceSolution.vue and imported in maincomponent

---
+++ src/components/ServiceSolution.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="up-title"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ServiceSolution'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
