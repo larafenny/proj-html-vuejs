ad40e6eada7f6d9f976bb4933f08ff93231a13ee
Lara Filippone <lara.filippone@gmail.com>
2022-04-28 10:19:20 +0200

    skinned AwardComponent

--- src/components/AwardComponent.vue
+++ src/components/AwardComponent.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>award</div>
+    <div class="container">
+        <div class="up-title">HEADACHE FREE</div>
+        <div class="title">Award Winning <br> Moving Company</div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptas distinctio praesentium sed laboriosam illum atque porro adipisci architecto, enim eligendi neque tempora accusantium? Blanditiis labore id ad iure alias.</p>
+        <button>READ OUR TESTIMONIALS</button>
+
+    </div>
 </template>
 
 <script>
@@ -9,6 +15,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/palette.scss";
+@import "../assets/elements.scss";
 
-</style>
+.container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 400px;
+    background-color: $lightGray;
+    padding: 0 10%;
+    
+}
+
+</style>>
+
