4c3136f04a89dae6b342ad3c669639e54005e75d
Lara Filippone <lara.filippone@gmail.com>
2022-04-28 22:40:53 +0200

    created CardComponent and imported in TestimonilComponent Skinned testimonialComponent

---
+++ src/components/CardTestimonial.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>CARD</div>
+</template>
+
+<script>
+export default {
+    name: 'CardTestimonial'
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/TestimonialComponent.vue
+++ src/components/TestimonialComponent.vue
@@ -1,14 +1,40 @@
 <template>
-    <div>TESTIMONIAL</div>
+    <div class="container">
+        <div class="up-title">HAPPY CUSTOMERS</div>
+        <div class="title">Testimonials</div>
+        <CardTestimonial />
+        <button>READ MORE TESTIMONIALS</button>
+    </div>
 </template>
 
 <script>
+import CardTestimonial from './CardTestimonial.vue'
+
 export default {
-    name: 'TestimonialComponent'
+    name: 'TestimonialComponent',
+    components: {
+        CardTestimonial
+    }
 
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../assets/palette.scss";
+@import "../assets/elements.scss";
+
+.container{
+    background-image: url("/src/images/avada-movers-testimonial-background.jpg");
+    background-position: center;
+    background-size: cover;
+    height: 400px;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+    
+
+    
 
 </style>
