9ea818bad1f68e73bb865618e1c1b24a609191e7
Lara Filippone <lara.filippone@gmail.com>
2022-04-29 17:15:48 +0200

    skinned cardTestimonial

--- src/components/CardTestimonial.vue
+++ src/components/CardTestimonial.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
-        <img src="" alt="">
+        <img src="@/images/avada-movers-johndoe-final.jpg" alt="">
+        <img v-bind:src="'@/images/' + testimonial.image" alt="">
         <h1>{{testimonial.name}}</h1>
         <p>{{testimonial.info}}</p>
     </div>
@@ -27,18 +28,24 @@ export default {
             width: 400px;
             text-align: center;
             img{
-                width: 100%;
+                width: 70%;
+                border-radius: 50%;
+                margin-bottom: 20px;
+
             }
             
                 h1{
                     color: $denim;
                     font-size: 20px;
                     font-weight: 900;
+                    margin-bottom: 20px;
+
                 }
                 p{
                     color: $shark;
                     font-size: 12px;
                     line-height: 1.8em;
+                    margin-bottom: 20px;
                 }
             
         }
